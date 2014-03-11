2ba47d67ba2f6fb0ff3490823a78a47666529f5f
Filip Zachar <tulak@netspace.sk>
2014-03-11 16:12:08 +0100

    Adds tests for MissionManagerImpl

---
+++ src/backend/MissionManagerImpl.java
@@ -0,0 +1,33 @@
+package backend;
+
+import java.util.List;
+
+/**
+ * Created by tulak on 11.03.2014.
+ */
+public class MissionManagerImpl implements MissionManager {
+    @Override
+    public Mission createMission(Mission mission) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Mission removeMission(Mission mission) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Mission> findAllMissions() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Mission findMission(Long id) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean updateMission(Mission mission) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/test/MissionManagerImplTest.java
@@ -0,0 +1,83 @@
+package test;
+
+import backend.Mission;
+import backend.MissionManager;
+import backend.MissionManagerImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tulak on 11.03.2014.
+ */
+public class MissionManagerImplTest {
+    private MissionManager manager;
+    @Before
+    public void setUp() throws Exception {
+        manager = new MissionManagerImpl();
+    }
+
+    @Test
+    public void testCreateMission() throws Exception {
+        Mission mission = newMission("Zabitie prezidenta", "USA", "Urobit co najtichsie", true);
+        manager.createMission(mission);
+        Long missionID = mission.getId();
+        assertNotNull(missionID);
+
+        Mission result = manager.findMission(missionID);
+        assertEquals(mission, result);
+        assertNotSame(mission, result);
+        assertDeepEquals(mission, result);
+    }
+
+    @Test
+    public void testRemoveMission() throws Exception {
+        Mission mission1 = newMission("Zabitie prezidenta", "USA", "Urobit co najtichsie", true);
+        Mission mission2 = newMission("Sledovacka", "Maroko", "Nezjest vsetky koblihy", false);
+        manager.createMission(mission1);
+        manager.createMission(mission2);
+
+        assertNotNull(manager.findMission(mission1.getId()));
+        assertNotNull(manager.findMission(mission2.getId()));
+
+        manager.removeMission(mission2);
+        assertNotNull(manager.findMission(mission1.getId()));
+        assertNull(manager.findMission(mission2.getId()));
+    }
+
+    @Test
+    public void testFindMission() throws Exception {
+        assertNull(manager.findMission(42l));
+        Mission mission = newMission("Zabitie prezidenta", "USA", "Urobit co najtichsie", true);
+        manager.createMission(mission);
+        Long missionID = mission.getId();
+        assertNotNull(missionID);
+
+        Mission result = manager.findMission(missionID);
+        assertEquals(mission, result);
+        assertDeepEquals(mission, result);
+    }
+
+    private Mission newMission(String name, String destination, String description, boolean secret) {
+
+        Mission tmp = new Mission();
+        tmp.setName(name);
+        tmp.setDestination(destination);
+        tmp.setDescription(description);
+        tmp.setSecret(secret);
+
+        return tmp;
+
+    }
+
+    private void assertDeepEquals(Mission expected, Mission actual) {
+
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getDescription(), actual.getDescription());
+        assertEquals(expected.getDestination(), actual.getDestination());
+        assertEquals(expected.isSecret(), actual.isSecret());
+
+    }
+}
