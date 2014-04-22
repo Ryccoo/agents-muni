49aa9c21f013724d469cb9e9b567947654511676
Filip Zachar <tulak@netspace.sk>
2014-04-22 11:26:23 +0200

    Loads jdbc path from environment variables in server listener

--- src/main/java/web/ServerListener.java
+++ src/main/java/web/ServerListener.java
@@ -2,21 +2,14 @@ package web;
 
 import backend.AgentManagerImpl;
 import backend.AssigmentManagerImpl;
-import backend.MissionManager;
 import backend.MissionManagerImpl;
-import db.AgentsTable;
 import db.CreateTables;
 import org.apache.commons.dbcp.BasicDataSource;
-import sun.management.resources.agent;
-import utils.DBUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 /**
  * Created by richard on 22.4.2014.
@@ -28,22 +21,18 @@ public class ServerListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent ev) {
         System.out.println("aplikace inicializována");
         ServletContext servletContext = ev.getServletContext();
-        AgentManagerImpl agentManager = new AgentManagerImpl();
         BasicDataSource ds;
-        ds = CreateTables.prepareDataSource("jdbc:derby:/home/richard/IdeaProjects/db/Agents;create=true");
+        ds = CreateTables.prepareDataSource(System.getenv("ISIS_JDBC_PATH"));
         ds.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
-        Connection conn;
-        try {
-            conn = ds.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+
         AgentManagerImpl agentm = new AgentManagerImpl();
         MissionManagerImpl mism = new MissionManagerImpl();
         AssigmentManagerImpl asigm = new AssigmentManagerImpl();
+
         agentm.setDataSource(ds);
         mism.setDataSource(ds);
         asigm.setDataSource(ds);
+
         servletContext.setAttribute("agentManager", agentm);
         servletContext.setAttribute("missionManager", mism);
         servletContext.setAttribute("assigmentManager", asigm);
